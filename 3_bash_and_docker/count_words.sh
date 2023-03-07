a17ac8c48e2fad4f727664d71b68b7c2193c8b8a
animih <anisimov.mi@phystech.edu>
2023-03-08 01:33:03 +0300

    added bash scripts

--- 3_bash_and_docker/count_words.sh
+++ 3_bash_and_docker/count_words.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Desc: Find out frequency of words in a file
 
-if [ $# -ne 2 ];
+if [ $# -ne 1 ];
 then
-  echo "Usage: $0 filename1, filename2";
+  echo "Usage: $0 filename1";
   exit -1
 fi
 
@@ -11,17 +11,4 @@ filename1=$1
 filename2=$2
 egrep -o "\b[[:alpha:]]+\b" $filename1 | \
 
-tr -sc '[:alnum:]' '\n'  < $filename1 | tr  '[:upper:'] '[:lower:]' | sort | uniq -c | tee count_words.txt
-IFS=$'\n'
-for name in $(sort -k 1 -n -r count_words.txt | head -10)
-do
-    IFS=$' '
-    read -r -a array <<< $name
-    num="${array[0]}"
-    word="${array[1]}"
-    IFS=$'\n'
-    dir_name="./${filename2}/${word}_${num}"
-    touch $dir_name
-done
-
-rm count_words.txt
+tr -sc '[:alnum:]' '\n'  < $filename1 | tr  '[:upper:'] '[:lower:]' | sort | uniq -c 
